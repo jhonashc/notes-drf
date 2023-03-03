3c18696f5e80b691fabe0f8c54cbc9914c36a19c
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-03 18:04:57 -0500

    pagination handler mixin added

---
+++ apps/base/mixins.py
@@ -0,0 +1,5 @@
+class PaginationHandlerMixin(object):
+    def paginate_queryset(self, queryset):
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
